53d8b7f8a6e23355d38f48d3eb18b4951271ebea
Arman Tavana <atava17@stlawu.edu>
2020-01-29 14:06:20 -0500

    fib1

--- javascript/statements.js
+++ javascript/statements.js
@@ -12,11 +12,11 @@ for (var i = 1; i <= 10; i++) {
 console.log(x);
 
 let j = 0;
-let i = 1;
+let i1 = 1;
 let k = 0;
-while (i < 1000) {
-  k = i + j;
-  j = i;
-  i = k;
+while (i1 < 1000) {
+  k = i1 + j;
+  j = i1;
+  i1 = k;
 }
 console.log(i);
