237462dda865efaa5aeec649cb8c17cc3d2007eb
Arman Tavana <atava17@stlawu.edu>
2020-01-29 13:47:35 -0500

    change

---
+++ javascript/expressions.js
@@ -0,0 +1,17 @@
+// expression syntax in JS
+
+// Booleans
+true
+false
+
+!false
+true && false
+true || false
+
+// numbers (all one type)
+
+// equality
+3 === 3.0
+3 !== 3.14
+
+// ordering
---
+++ javascript/statements.js
@@ -0,0 +1,6 @@
+
+const pi = 3.14;
+
+let x = 0;
+
+console.log('hiiii');
