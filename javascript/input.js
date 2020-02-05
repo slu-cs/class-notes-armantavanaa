f94fd6726580a90aec54c4151f057bd839c30b5e
Arman Tavana <atava17@stlawu.edu>
2020-02-05 13:50:10 -0500

    inp3

--- javascript/input.js
+++ javascript/input.js
@@ -2,7 +2,7 @@ const readline = require('readline');
 
 const user = readline.createInterface({
   input: process.stdin,
-  output: process.stout
+  output: process.stdout
 });
 
 user.question('Filename: ', function(filename){
