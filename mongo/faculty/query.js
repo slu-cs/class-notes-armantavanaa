618759f81317d2c0c99b3a57294e09aa3ed8c4a1
Arman Tavana <armantavana@Armans-MacBook-Pro.local>
2020-02-12 13:25:16 -0500

    db

--- mongo/faculty/query.js
+++ mongo/faculty/query.js
@@ -6,9 +6,32 @@ const Professor = require('./schema');
 
 connect(); // To the database
 
-// What documents are in the collection?
+/* What documents are in the collection?
 const query = Professor.find();
 query.exec(function(error, professors) {
   if (error) console.error(error.stack);
   console.log(professors);
 });
+*/
+
+const queries = [
+  // what are names in alphabetic order?
+  Professor.find().sort('name'),
+
+  //who started most recently
+
+  Professor.find().sort('.started').limit(1),
+
+  // who started in 2002
+  Professor.find().where('.started').equals(2003),
+
+  //who teaches 362
+
+  Professor.find().where('courses').in(362),
+
+  //what are all the ranks?
+
+  Professor.distinct('rank')
+
+
+]
